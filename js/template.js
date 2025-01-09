a4674c918da93de81d9b16af39969ce8fb7845bb
sebaslizano7194 <86936378+sebaslizano7194@users.noreply.github.com>
2025-01-09 09:56:17 -0600

    Update template.js

--- js/template.js
+++ js/template.js
@@ -399,7 +399,7 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-  let birthday = "Feb 08, 2025 15:00:00",
+  let birthday = "Feb 14, 2025 15:30:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
         let now = new Date().getTime(),
